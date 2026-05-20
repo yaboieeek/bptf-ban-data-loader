f579547d3405930574ff35878073ad9595e6de78
yaboieeek <typarasa@gmail.com>
2026-05-20 16:06:04 +0300

    Initial commit

--- banDataLoader.user.js
+++ banDataLoader.user.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=backpack.tf
 // ==/UserScript==
 
+
 const toPerform = () => {
     const element = document.querySelector('.alert.alert-danger');
 
